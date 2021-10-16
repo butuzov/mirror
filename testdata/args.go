47e59ef31ee91d495d9c60da26b8c33e2d1107b2
Oleg Butuzov <butuzov@made.ua>
2021-10-16 06:34:18 +0300

    test: arguments tests

---
+++ testdata/args.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+)
+
+const s3 string = "a"
+
+func m() {
+	golden := "foobar"
+
+	// pointer to ...
+	// {
+	// 	s0 := []byte("foobar")
+	// 	s1 := &s0
+	// 	_ = strings.IndexAny(string(*s1), "foobar")  want `avoid allocations with strings\.IndexAny`
+	// }
+	{
+		const s0 = "a"
+		_ = bytes.IndexAny([]byte(s0), golden) // want `avoid allocations with strings\.IndexAny`
+	}
+	{
+		const s0 string = "a"
+		_ = bytes.IndexAny([]byte(s0), golden) // want `avoid allocations with strings\.IndexAny`
+	}
+	{
+		_ = bytes.IndexAny([]byte(s3), golden) // want `avoid allocations with strings\.IndexAny`
+	}
+
+	// constant (string)
+	{
+		var s0 string = "a"
+		_ = bytes.IndexAny([]byte(s0), golden) // want `avoid allocations with strings\.IndexAny`
+	}
+	{
+		const s0 = "foo"
+		_ = bytes.IndexAny([]byte(s0), golden) // want `avoid allocations with strings\.IndexAny`
+	}
+	// function return string
+	{
+		s0 := func() string { return golden }
+		_ = bytes.IndexAny([]byte(s0()), golden) // want `avoid allocations with strings\.IndexAny`
+	}
+	// function return byte
+	{
+		s0 := func() []byte { return []byte(golden) }
+		_ = strings.IndexAny(string(s0()), golden) // want `avoid allocations with bytes\.IndexAny`
+	}
+	{
+		s0 := [][]byte{[]byte("foobar")}
+		_ = strings.IndexAny(string(s0[0]), golden) // want `avoid allocations with bytes\.IndexAny`
+	}
+	{
+		s0 := "foo"
+		s1 := "foo"
+		_ = bytes.IndexAny([]byte(s0+s1), golden) // want `avoid allocations with strings\.IndexAny`
+	}
+	{
+		s0 := []string{"foobar"}
+		_ = bytes.IndexAny([]byte(s0[0]), golden) // want `avoid allocations with strings\.IndexAny`
+	}
+}
